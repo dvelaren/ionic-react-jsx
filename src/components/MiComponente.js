ab48efcbc1b55ef589df067a9b0a1f79ba56495f
David Velasquez <dvelasquez@vicomtech.org>
2020-04-10 11:50:34 +0200

    Improved layout distribution

--- src/components/MiComponente.js
+++ src/components/MiComponente.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react'
-import { IonItem, IonList, IonLabel, IonHeader } from '@ionic/react'
+import { IonPage, IonItem, IonList, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react'
 
 import './MiComponente.css'
 
 export default class MiComponente extends Component {
-    render() {
-        console.log(this.props.var1)
-        return (
-            <div>
-                <IonList>
-                    <IonHeader className="miClase">{this.props.titulo}</IonHeader>
-                    <IonItem>
-                        <IonLabel>Hola me llamo {this.props.nombre}</IonLabel>
-                    </IonItem>
-                </IonList>
-
-            </div>
-        )
-    }
+  render() {
+    console.log(this.props.var1)
+    return (
+      <div>
+        <IonPage>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>Tab 1</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent>
+            <IonList>
+              <IonHeader className="miClase">{this.props.titulo}</IonHeader>
+              <IonItem>
+                <IonLabel>Hola me llamo {this.props.nombre}</IonLabel>
+              </IonItem>
+            </IonList>
+          </IonContent>
+        </IonPage>
+      </div>
+    )
+  }
 }
